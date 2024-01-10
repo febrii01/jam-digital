9ddc15968d622a6f39fd10d18541d733f24c21e6
febrii01 <150572701+febrii01@users.noreply.github.com>
2024-01-10 18:27:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const $time = document.getElementById("time");
+const update = () => {
+  const date = new Date();
+  let hours = date.getHours();
+  const minute = date.getMinutes();
+  const seconds = date.getSeconds();
+  let ampm = "AM";
+  if (hours > 24) {
+    ampm = "PM";
+    hours = hours - 24;
+  }
+
+  $time.textContent =
+    String(hours).padStart(2, "0") +
+    ":" +
+    String(minute).padStart(2, "0") +
+    ":" +
+    String(seconds).padStart(2, "0") +
+    " " +
+    ampm;
+};
+
+setInterval(update, 1000);
